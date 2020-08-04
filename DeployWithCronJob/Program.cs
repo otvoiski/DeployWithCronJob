a36390e9a3b61833462d99e59a2ed108ea942761
Otavio Voiski <otvoiski@live.com>
2020-08-04 00:11:31 -0300

    Add project files.

---
+++ DeployWithCronJob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeployWithCronJob
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"This runned in {DateTime.Now}");
+        }
+    }
+}
